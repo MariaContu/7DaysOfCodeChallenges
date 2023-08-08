9122595dedb63f8162f70a5fc93f472c40380ea1
MariaContu <mariaescontu@gmail.com>
2023-08-08 17:40:21 -0300

    tuts

--- Java - Paulo Silveira/DayOne.java
+++ Java - Paulo Silveira/DayOne.java
@@ -1,7 +1,22 @@
-/**
- * DayOne
- */
-public class DayOne {
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 
-    
+class SevenDaysOfCodeJavaDay1 {
+
+	public static void main(String[] args) throws Exception {
+
+		String apiKey = "k_12345678";
+		URI apiIMDB = URI.create("https://imdb-api.com/en/API/Top250TVs/" + apiKey);
+		
+		HttpClient client = HttpClient.newHttpClient();
+		HttpRequest request = HttpRequest.newBuilder().uri(apiIMDB).build();
+
+		HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+		String json = response.body();
+
+		System.out.println("Resposta: " + json);
+	}
+
 }
--- JavaScript - Ballerini/Day2.js
+++ JavaScript - Ballerini/Day2.js
@@ -4,7 +4,7 @@ const linguagem = prompt("Qual linguagem de programação você está estudando?
 
 alert("Olá " + nome + ", você tem " + idade + " anos e já está aprendendo " + linguagem + "!")
 
-const like = prompt("Você gosta de estudar "+ [linguagem] +"? Responda com o número 1 para SIM ou 2 para NÃO.")
+const like = prompt("Você gosta de estudar "+ linguagem +"? Responda com o número 1 para SIM ou 2 para NÃO.")
 if (like == 1) {
     alert("Muito bom! Continue estudando e você terá muito sucesso.")
 }
